4960111a30ecaf35845ce1a7da7d7fcc6da7c367
bogdanovpd@gmail.com <BogdanovPD>
2019-05-26 13:32:08 +0300

    webflux provided

--- src/main/java/ru/otus/spring/libr/repository/BookRepository.java
+++ src/main/java/ru/otus/spring/libr/repository/BookRepository.java
@@ -1,18 +1,18 @@
 package ru.otus.spring.libr.repository;
 
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
 import ru.otus.spring.libr.entities.Book;
 
-import java.util.List;
-
-public interface BookRepository extends MongoRepository<Book, String> {
-
-    List<Book> findAllByNameAndAuthor(String name, String author);
-    List<Book> findAllByNameAndGenre(String name, String genre);
-    List<Book> findAllByAuthorAndGenre(String author, String genre);
-    List<Book> findAllByNameAndAuthorAndGenre(String name, String author, String genre);
-    List<Book> findAllByName(String name);
-    List<Book> findAllByAuthor(String author);
-    List<Book> findAllByGenre(String genre);
+@Repository
+public interface BookRepository extends ReactiveMongoRepository<Book, String> {
+    Flux<Book> findAllByNameAndAuthor(String name, String author);
+    Flux<Book> findAllByNameAndGenre(String name, String genre);
+    Flux<Book> findAllByAuthorAndGenre(String author, String genre);
+    Flux<Book> findAllByNameAndAuthorAndGenre(String name, String author, String genre);
+    Flux<Book> findAllByName(String name);
+    Flux<Book> findAllByAuthor(String author);
+    Flux<Book> findAllByGenre(String genre);
 }
 
